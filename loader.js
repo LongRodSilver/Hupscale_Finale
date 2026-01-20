8aa380b6dfa57d00b4360221afe5eeae91d03f72
Hupscale Deploy <deploy@hupscale.com>
2026-01-20 06:44:31 -0500

    Replace video with gallery carousel and add Livre d'or button

--- loader.js
+++ loader.js
@@ -1,3 +1,3 @@
 export default function myImageLoader({ src, width, quality }) {
-  return `https://longrodsilver.github.io/Hupscale${src}?w=${width}&q=${quality || 75}`
+  return `https://longrodsilver.github.io/Hupscale_Finale${src}?w=${width}&q=${quality || 75}`
 }
